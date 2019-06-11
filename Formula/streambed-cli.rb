56e175dfe73d027ccbf40f045610f74f29e590f5
Streambed Tools (CircleCI) <streambed.tools@gmail.com>
2019-06-11 21:25:30 +0000

    Pushing given builds commit f0766d1 - Built streambed-cli 0.24.0

--- Formula/streambed-cli.rb
+++ Formula/streambed-cli.rb
@@ -1,7 +1,7 @@
 class StreambedCli < Formula
-  url "https://repositories.streambed.io/packages/streambed-cli/0.23.0/streambed-cli-0.23.0-mac-x86_64.tar.gz", :using => :nounzip
-  version "0.23.0"
-  sha256 "7426e57b09206dcb79c39b54c41565b2cf3117b33c00a067c900cc64a7f74a1e"
+  url "https://repositories.streambed.io/packages/streambed-cli/0.24.0/streambed-cli-0.24.0-mac-x86_64.tar.gz", :using => :nounzip
+  version "0.24.0"
+  sha256 "298d9774bd455bc7d5ba4d4db3f0241e5a10b560bcf1b936c934e22556c9edc7"
 
   desc "CLI tools to install and manage Streambed applications"
   homepage "https://www.streambed.io/"
